e7acf146ec8ec9c8c2fcf61f9ca2e074e2e89bb3
Md. Shahriar Kabir <shahriarkawsik@gmail.com>
2025-06-13 08:46:20 +0600

    add font

---
+++ src/app/Font/Font.ts
@@ -0,0 +1,10 @@
+import { Roboto, Poppins } from "next/font/google";
+
+export const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
+export const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
