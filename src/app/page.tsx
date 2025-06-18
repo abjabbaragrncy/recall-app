b5498678009912c779487be9740b4779db568b96
abnahid <aznahid507@gmail.com>
2025-06-19 00:32:50 +0600

    about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+import Calltoaction from "@/components/Calltoaction";
+import { manrope, poppins } from "@/Font/font";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>this is Home page.</h1>
+    <div className={`${manrope.variable} ${poppins.variable} `}>
+      <h1 className="p-24 text-xl text-center ">this is Home page.</h1>
+      <div className="max-w-7xl  py-[60px] bg-white flex flex-col gap-10 ">
+        <h2 className="text-[#131314] text-2xl md:text-[32px] font-semibold font-poppins leading-[1.5] max-w-[628px]">
+          Explore exciting more features on our mobile application
+        </h2>
+        <div></div>
+      </div>
+
+      <Calltoaction />
     </div>
   );
 }
