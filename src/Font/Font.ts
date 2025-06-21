95d1779df87e861939fc2ce0916c3421bf05191f
abnahid <aznahid507@gmail.com>
2025-06-21 23:45:08 +0600

    Responcibe about page

--- src/Font/Font.ts
+++ src/Font/Font.ts
@@ -1,5 +1,10 @@
+import { Manrope, Poppins, Roboto_Flex } from "next/font/google";
 
-import { Manrope, Poppins } from "next/font/google";
+export const roboto_flex = Roboto_Flex({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  variable: "--font-roboto-flex",
+});
 
 export const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -12,4 +17,3 @@ export const manrope = Manrope({
   subsets: ["latin"],
   variable: "--font-manrope",
 });
-
